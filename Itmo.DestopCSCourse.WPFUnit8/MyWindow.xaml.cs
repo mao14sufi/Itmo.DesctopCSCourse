63a4b26feaae00607efbf60e3583c4c01d148161
an ma <tommycveigh@gmail.com>
2024-07-31 15:31:44 +0300

    add

---
+++ Itmo.DestopCSCourse.WPFUnit8/MyWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Itmo.DesktopCSCourse.WPFUnit1
+{
+    /// <summary>
+    /// Interaction logic for MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+        private bool _close;
+        MainWindow wnd1 = null;
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+        public new void Close()
+        {
+            _close = true;
+            base.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_close) return;
+            e.Cancel = true;
+            Hide();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null)
+            {
+
+                wnd1.txtBlock.Text =  listBox1.SelectedItem  + "  " + textBox.Text;
+                PrintLogFile();
+
+            }
+            Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            wnd1.myWin = null;
+        }
+
+       // System.IO.StreamWriter writer = new System.IO.StreamWriter("D:\\log.txt",true);
+       
+        private void PrintLogFile()
+        {
+            System.IO.StreamWriter writer = new System.IO.StreamWriter("D:\\log.txt", true);
+            writer.WriteLine("Внесено {0}: {1} ", 
+            textBox.Text, DateTime.Now.ToShortDateString() + ", время: " +
+            DateTime.Now.ToLongTimeString());
+            writer.Flush();
+
+
+        }
+
+        
+    }
+}
---
+++ Itmo.DestopCSCourse.WPFUnit8/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace Itmo.DesktopCSCourse.WPFUnit1
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+        public Student(string name, bool ch)
+        {
+            StudentName = name;
+            IsEnrolled = ch;
+        }
+
+        public override string ToString()
+        {
+            return StudentName + "\t" + IsEnrolled;
+
+        }
+
+
+
+        public string FullStudentData
+        {
+            get { return StudentName + "\t" + IsEnrolled; }
+        }
+    }
+    public class StudentList : ObservableCollection<Student>
+    {
+        public StudentList()
+        {
+            Add(new Student("Lorin Kanev", true));
+            Add(new Student("Ivan Petrov", true));
+            Add(new Student("Sergey Masov", false));
+            Add(new Student("Tais Frolova", true));
+            Add(new Student("Elena Diva", false));
+        }
+    }
+
+}
